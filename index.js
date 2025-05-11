aa3a21d49c0cce47a520f123192fb542c677e9f7
Michael McCormack <mcctwin@gmail.com>
2025-05-11 16:57:22 -0500

    8 tests passed, 5 tests failed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"]
+let featuredDrink = "Strawberry Milkshake"
+
+function addBurger(){
+    const newBurger = "Flatburger"
+    burgers.push(newBurger)
+    if (burgers[0] !== "Maple Bacon Burger"){
+        const addAnotherBurger = "Maple Bacon Burger"
+        burgers.push(addAnotherBurger)
+    }
+}
+function changFeaturedDrink(){
+    featuredDrink = "The JavaShake"
+}
+
